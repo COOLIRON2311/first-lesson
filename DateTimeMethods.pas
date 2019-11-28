8113d5e41d5d4050cb201b9dbc11d4162bfc179b
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2019-11-28 16:24:45 +0300

    Улучшена функция DaysInMonth

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -4,18 +4,9 @@ function IsLeapYear(year: integer): boolean := year mod 4 = 0;
 function DaysInMonth(month, year: integer): integer;
 begin
   case month of
-    1: result := 31;
+    1, 3, 5, 7, 8, 10, 12: result := 31;
+    4, 6, 9, 11: result := 30;
     2: result := IsLeapYear(year) ? 29 : 28;
-    3: result := 31;
-    4: result := 30;
-    5: result := 31;
-    6: result := 30;
-    7: result := 31;
-    8: result := 31;
-    9: result := 30;
-    10: result := 31;
-    11: result := 30;
-    12: result := 31;
   end;
 end;
 function LaterInDay(p1, p2: DateTime): DateTime := p1 < p2 ? p1 : p2;
